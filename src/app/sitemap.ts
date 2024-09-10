b2c7cf70cfec1a3852d8f2de8ca072fa1139967b
M4X809 <github@j-pet.de>
2024-09-11 00:45:10 +0200

    Add sitemap generation for website URLs with metadata attributes

---
+++ src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+	return [
+		{
+			url: "https://max809.de",
+			lastModified: new Date(),
+			changeFrequency: "daily",
+			priority: 1,
+		},
+		{
+			url: "https://max809.de/qr-code-generator",
+			lastModified: new Date(),
+			changeFrequency: "daily",
+			priority: 0.8,
+		},
+	];
+}
