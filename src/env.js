a669e964e06dd8dfdd187170a502cb29f25e991e
M4X809 <github@j-pet.de>
2024-09-11 11:20:03 +0200

    modified:   src/env.js

--- src/env.js
+++ src/env.js
@@ -13,13 +13,14 @@ export const env = createEnv({
 			.default("development"),
 		NEXTAUTH_SECRET:
 			process.env.NODE_ENV === "production" ? z.string() : z.string().optional(),
-		NEXTAUTH_URL: z.preprocess(
-			// This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-			// Since NextAuth.js automatically uses the VERCEL_URL if present.
-			(str) => process.env.COOLIFY_URL ?? str,
-			// VERCEL_URL doesn't include `https` so it cant be validated as a URL
-			process.env.COOLIFY_CONTAINER_NAME ? z.string() : z.string().url(),
-		),
+		// NEXTAUTH_URL: z.preprocess(
+		// 	// This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+		// 	// Since NextAuth.js automatically uses the VERCEL_URL if present.
+		// 	(str) => process.env.COOLIFY_URL ?? str,
+		// 	// VERCEL_URL doesn't include `https` so it cant be validated as a URL
+		// 	process.env.COOLIFY_URL ? z.string() : z.string().url(),
+		// ),
+		NEXTAUTH_URL: z.string(),
 		DISCORD_CLIENT_ID: z.string(),
 		DISCORD_CLIENT_SECRET: z.string(),
 
@@ -46,7 +47,7 @@ export const env = createEnv({
 		DATABASE_URL: process.env.DATABASE_URL,
 		NODE_ENV: process.env.NODE_ENV,
 		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+		NEXTAUTH_URL: process.env.COOLIFY_URL ?? process.env.NEXTAUTH_URL,
 		DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
 		DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
 
