8a4a2956ef12d04e5637a8dc64149c8fe232ff07
M4X809 <github@j-pet.de>
2024-09-11 09:51:12 +0200

    Update env.js to replace VERCEL_URL with COOLIFY_URL for better environment variable handling in deployments

--- src/env.js
+++ src/env.js
@@ -16,9 +16,9 @@ export const env = createEnv({
 		NEXTAUTH_URL: z.preprocess(
 			// This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
 			// Since NextAuth.js automatically uses the VERCEL_URL if present.
-			(str) => process.env.VERCEL_URL ?? str,
+			(str) => process.env.COOLIFY_URL ?? str,
 			// VERCEL_URL doesn't include `https` so it cant be validated as a URL
-			process.env.VERCEL ? z.string() : z.string().url(),
+			process.env.COOLIFY_CONTAINER_NAME ? z.string() : z.string().url(),
 		),
 		DISCORD_CLIENT_ID: z.string(),
 		DISCORD_CLIENT_SECRET: z.string(),
