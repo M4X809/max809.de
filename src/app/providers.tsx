2a670a14230516940e3e9ae40cf23774091cc411
M4X809 <github@j-pet.de>
2024-09-13 15:35:31 +0200

    TEST REMOVE env.js

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,13 @@
 'use client'
 import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
-import { env } from '~/env'
+// import { env } from '~/env'
 
 if (typeof window !== 'undefined') {
-  posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+  if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+    throw new Error("NEXT_PUBLIC_POSTHOG_KEY is not set")
+  }
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: "/ingest",
     person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
     autocapture: true,
