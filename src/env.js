960316131aca4353fa7af766062639fa9e5bfc37
M4X809 <github@j-pet.de>
2024-09-13 12:51:14 +0200

    added debug

--- src/env.js
+++ src/env.js
@@ -1,6 +1,8 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
+console.log("next auth url", process.env.NEXTAUTH_URL);
+
 export const env = createEnv({
 	/**
 	 * Specify your server-side environment variables schema here. This way you can ensure the app
