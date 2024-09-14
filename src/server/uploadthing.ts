921c4a845793964b6895d1af841f42359c78b4ec
M4X809 <github@j-pet.de>
2024-09-14 19:15:57 +0200

    Change log level from "All" to "Info" in uploadthing server configuration for reduced verbosity and better logging clarity

--- src/server/uploadthing.ts
+++ src/server/uploadthing.ts
@@ -2,6 +2,6 @@ import { UTApi } from "uploadthing/server";
 import { env } from "~/env";
 export const utapi = new UTApi({
 	token: env.UPLOADTHING_TOKEN,
-	logLevel: "All",
+	logLevel: "Info",
 	defaultKeyType: "fileKey",
 });
