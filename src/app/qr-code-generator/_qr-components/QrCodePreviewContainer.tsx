a2668e9507ca584168866fd51c011d1d33713bca
M4X809 <github@j-pet.de>
2024-09-15 12:11:58 +0200

    Add delete functionality for QR codes with confirmation modal (#6)
    * Add tailwind-merge package and enhance QR code component for improved functionality, including shareable links and timestamps
    * Enable production source maps in next.config.js for improved debugging in production environments
    * Add delete functionality for QR codes with confirmation modal and error handling in the QR code preview component
    * Bump version to 0.2.0

--- src/app/qr-code-generator/_qr-components/QrCodePreviewContainer.tsx
+++ src/app/qr-code-generator/_qr-components/QrCodePreviewContainer.tsx
@@ -1,14 +1,17 @@
 "use client"
 import type React from 'react'
-import { useEffect, useState } from 'react'
+import { use, useEffect, useState } from 'react'
 import QrCodePreview from "./QrCodePreview"
-import { Container, Grid, GridCol, Group, Stack, Text, Title } from '@mantine/core'
+import { ActionIcon, Box, Button, Container, Group, Modal, Paper, Pill, Stack, Text, Title, Tooltip } from '@mantine/core'
 import { api } from '~/trpc/react'
 import { useAppStore } from '~/providers/app-store-provider'
-import { useClipboard } from '@mantine/hooks'
+import { useClipboard, useDisclosure } from '@mantine/hooks'
 
-import { env } from "~/env"
 import LoadQrConfig from './LoadQrConfig'
+import { faArrowUpRightFromSquare, faTrashCan } from '@fortawesome/pro-duotone-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { twMerge } from 'tailwind-merge'
+
 
 interface QrCodePreviewContainerProps {
     codes: {
@@ -29,17 +32,36 @@ interface QrCodePreviewContainerProps {
     }[],
     limits: { current: number, max: number }
     userId: string
+    baseURL: string
 }
 
-const QrCodePreviewContainer: React.FC<QrCodePreviewContainerProps> = ({ codes, limits, userId }) => {
+const QrCodePreviewContainer: React.FC<QrCodePreviewContainerProps> = ({ codes, limits, userId, baseURL }) => {
 
     const { data, isLoading, isError, refetch, } = api.codes.getQrCodes.useQuery(undefined, { initialData: { codes, limits }, enabled: !!userId })
+
+    const { mutate: deleteCode, isPending: isDeleting, isError: isDeleteError, isSuccess: isDeleteSuccess, error: deleteError, reset } = api.codes.deleteQrCode.useMutation()
+
     const refetchCodes = useAppStore((state) => state.refetchCodes)
+
+    const [opened, { toggle, close }] = useDisclosure(false)
+    const [deleteCodeId, setDeleteCodeId] = useState<string | null>(null)
+    const [deleteName, setDeleteName] = useState<string | null>(null)
+
 
     // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
     useEffect(() => {
         refetch()
     }, [refetchCodes])
+
+    useEffect(() => {
+        if (isDeleteSuccess) {
+            setDeleteCodeId(null)
+            setDeleteName(null)
+            refetch()
+            close()
+        }
+    }, [close, refetch, isDeleteSuccess])
+
 
     const { copied, copy } = useClipboard({ timeout: 500 })
     const [copiedName, setCopiedName] = useState<string | null>(null)
@@ -51,30 +73,114 @@ const QrCodePreviewContainer: React.FC<QrCodePreviewContainerProps> = ({ codes, 
     const QrCodes = data.codes.map((code) => {
 
 
-        return (
-            <Container key={code.id} bg={"rgba(255,255,255,0.1)"} p={20} size={"sm"} w={450} className="rounded-lg">
-                <Grid columns={5}>
-                    <GridCol span={"auto"}>
-                        <Stack gap={0}>
-                            <Title order={2}>{code.name}</Title>
-                            <Text c={"dimmed"}> Value: <Text c={"white"} component="span" truncate>"{code.qrCode}"</Text></Text>
+        const DateElement = () => {
+            if (!code.createdById) return <Text c={"dimmed"}>Unknown</Text>
+            const date = new Date(code.updatedAt as any)
+            return <Text component='span' c={"dimmed"}>{date.toLocaleTimeString("de-DE")}</Text>
+        }
 
-                        </Stack>
-                    </GridCol>
-                    <GridCol span={"content"} mx={20} className='rounded-xl' >
-                        <QrCodePreview data={code} />
-                    </GridCol>
-                    <Group justify="space-between" w={"100%"} p={0}>
-                        <LoadQrConfig data={code} />
-                        <Text fz={11} c={copied && copiedName === code.name && code.shareable ? "white" : "dimmed"} td={"underline"} className={code.shareable ? 'cursor-pointer' : ''} onClick={() => {
-                            if (!code.shareable) return
-                            setCopiedName(code.name)
-                            copy(`${env.NEXT_PUBLIC_NODE_ENV === "development" ? "http://localhost:3000" : "https://max809.de"}/qr-code-generator/${code.id}`)
-                        }}>
-                            {code.shareable && <Text fz={11} component='span'> Click to Share</Text>} {code.id}
-                        </Text>
+        return (
+            <Container key={code.id} bg={"rgba(255,255,255,0.1)"} p={20} size={"sm"} w={500} maw={"100dvw"} className="rounded-lg">
+
+
+                <Group justify='space-between' grow gap={0} mah={500} wrap='nowrap'>
+                    <Stack gap={0}>
+                        <Title order={2}>{code.name}</Title>
+                        <Tooltip bg={"transparent"} position='bottom' label={
+                            <Paper
+                                p={15}
+                                w={200}
+                                h={"auto"}
+                                withBorder
+                                className='rounded-xl bg-gradient-to-tr from-[#222840] to-[#2347a1]'
+                                c={"white"}
+
+                                styles={{
+                                    root: {
+                                        wordBreak: "break-all",
+                                        textWrap: "wrap"
+                                    },
+                                }}>
+                                {code.qrCode}
+                            </Paper>}
+                        >
+                            <Text truncate w={350} c={"dimmed"}> Value: {code.qrCode}</Text>
+                        </Tooltip>
+                        <Text className='text-nowrap' c={"dimmed"}> Created / Updated At:  <DateElement /></Text>
+                    </Stack>
+                    <QrCodePreview data={code} w={100} />
+                </Group>
+                <Group id='QR-Code-Actions-Bottom' justify='space-between' align='center' >
+                    <Group id='QR-Code-Actions-Buttons' wrap='nowrap' >
+                        <Tooltip
+                            transitionProps={{ transition: "fade", }}
+                            classNames={{
+                                tooltip: 'bg-gradient-to-tr from-[#222840] to-[#2347a1] text-white'
+                            }} label='Share QR Code'
+                        >
+                            <Box>
+                                <LoadQrConfig data={code} variant='light' />
+                            </Box>
+                        </Tooltip>
+                        <Tooltip
+                            w={200}
+                            maw={"100dvw"}
+                            styles={{
+                                tooltip: {
+                                    wordBreak: "break-word",
+                                    textWrap: "wrap"
+                                },
+                            }}
+                            events={{ hover: true, focus: true, touch: !code.shareable }}
+                            transitionProps={{ transition: "fade", }}
+                            classNames={{
+                                tooltip: 'bg-gradient-to-tr from-[#222840] to-[#2347a1] text-white border border-[#4b4b4b]'
+                            }} label={
+                                code.shareable ? 'Copy QR Code link.' : <Text fz={13} w={"auto"}
+                                    maw={"100dvw"} >
+                                    This QR Code is not shareable. <br />
+                                    Use the arrow button to load it into the QR Code Generator, then save it with sharing enabled.
+                                </Text>
+                            }
+                        >
+                            <ActionIcon
+                                variant='light'
+                                className={twMerge("transition-colors duration-500", copied && copiedName === code.name && code.shareable ? "bg-green-800 text-green-200 hover:bg-green-800 hover:text-green-200" : "")}
+                                disabled={!code.shareable}
+                                onClick={() => {
+                                    setCopiedName(code.name)
+                                    copy(`${baseURL}/qr-code-generator/${code.id}`)
+                                }}>
+                                <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+                            </ActionIcon>
+                        </Tooltip>
+                        <Tooltip label='Delete QR Code' transitionProps={{ transition: "fade", }}>
+                            <ActionIcon
+
+                                variant='light'
+                                color={"red"}
+                                onClick={() => {
+                                    // console.log("delete", code.id)
+                                    setDeleteCodeId(code.id)
+                                    setDeleteName(code.name)
+                                    toggle()
+                                }}
+
+                            >
+                                <FontAwesomeIcon icon={faTrashCan} />
+                            </ActionIcon>
+                        </Tooltip>
+
                     </Group>
-                </Grid>
+                    <Text fz={11} c={"dimmed"} >
+                        {code.id}
+                    </Text>
+                </Group>
+
+
+
+
+
             </Container>
         )
     })
@@ -87,6 +193,68 @@ const QrCodePreviewContainer: React.FC<QrCodePreviewContainerProps> = ({ codes, 
             <Group wrap="wrap" grow justify="center">
                 {QrCodes}
             </Group>
+            <Modal centered
+                overlayProps={{
+                    blur: 2,
+                }}
+                classNames={{
+                    body: "bg-gradient-to-tr from-[#06080f] to-[#122b69] text-white",
+                }}
+                opened={opened && !!deleteCodeId}
+                withCloseButton={false}
+                onClose={() => {
+                    toggle()
+                    reset()
+                }} >
+                <Stack gap={5}>
+                    <Title order={2} ta='center' className='text-white'>
+                        Delete QR Code
+                    </Title>
+                    <Text>
+                        Are you sure you want to delete this QR Code?
+                    </Text>
+                    <Pill
+                        className='bg-[rgba(255,255,255,0.1)] '
+                        variant='contrast' c={"red"}
+                        radius={5}>
+                        Name:  {deleteName} <br />
+                        ID:  {deleteCodeId}
+                    </Pill>
+
+                    <Text fz={13} c={"red"} mt={10}>
+                        This action cannot be undone.
+                    </Text>
+
+                    <Button
+                        loading={isDeleting}
+                        color='red'
+                        variant='light'
+                        leftSection={<FontAwesomeIcon icon={faTrashCan} />}
+                        onClick={() => {
+                            if (!deleteCodeId || !deleteName) return
+                            deleteCode({ id: deleteCodeId })
+
+                        }}
+                    >
+                        Delete
+                    </Button>
+
+                </Stack>
+                <Box
+                    mt={10}
+
+
+                    w={"100%"}
+                    style={{ borderRadius: "5px" }}
+                    p={"sm"}
+                    c={"#fa2113"}
+                    bg={"#0D1117"}
+                    hidden={!isDeleteError}
+                >
+                    {deleteError?.message}
+                </Box>
+
+            </Modal>
         </>
     )
 }
