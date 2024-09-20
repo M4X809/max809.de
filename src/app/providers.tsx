27faae7d45ad463fffe600fc8a42d4657f51eba6
M4X809 <github@j-pet.de>
2024-09-20 08:35:17 +0200

    Update PostHog configuration to always create profiles for anonymous users

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,14 +6,13 @@ import { env } from '~/env'
 if (typeof window !== 'undefined') {
   posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: "/ingest",
-    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
     autocapture: true,
     capture_heatmaps: true,
     enable_heatmaps: true,
     disable_compression: false,
     enable_recording_console_log: false,
     cross_subdomain_cookie: true,
-
   })
 }
 export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
