642df1a3b2fdcbba447790ee636209ade5370ba4
M4X809 <github@j-pet.de>
2024-11-19 19:10:00 +0100

    Changed Naming in Dash Nav

--- src/app/(staff)/dashboard/_dash-components/DashNav.tsx
+++ src/app/(staff)/dashboard/_dash-components/DashNav.tsx
@@ -1,70 +1,77 @@
-"use client"
-import { faFeed, faFileContract, faHome, faList, faScrewdriverWrench, faUser } from "@fortawesome/pro-duotone-svg-icons"
-import { Box, Container, type MantineSize } from "@mantine/core"
-import type { Session } from "next-auth"
-import NavSidebar, { type NavMenuItemProps } from "~/app/_components/NavSidebar"
+"use client";
+import {
+	faFeed,
+	faFileContract,
+	faHome,
+	faList,
+	faScrewdriverWrench,
+	faUser,
+} from "@fortawesome/pro-duotone-svg-icons";
+import { Box, Container, type MantineSize } from "@mantine/core";
+import type { Session } from "next-auth";
+import NavSidebar, {
+	type NavMenuItemProps,
+} from "~/app/_components/NavSidebar";
 
 const DashNav = ({ session }: { session: Session | null | undefined }) => {
-    const elements = (): NavMenuItemProps[] => {
-        const data: NavMenuItemProps[] = [
-            {
-                type: "divider",
-                label: "Info",
-                icon: faHome,
-                requireStaff: true,
-            },
-            {
-                type: "item",
-                label: "Dashboard",
-                to: "/dashboard",
-                icon: faScrewdriverWrench,
-                requireStaff: true,
-            },
-            {
-                type: "item",
-                label: "User Management",
-                to: "/dashboard/user-management",
-                icon: faUser,
-                permission: "viewUser",
-            },
-            {
-                type: "item",
-                label: "Login Whitelist",
-                to: "/dashboard/login-whitelist",
-                icon: faList,
-                permission: "viewWhitelist",
-            },
-            {
-                type: "divider",
-                label: "Logbook",
-                icon: faFileContract,
-            },
-            {
-                type: "multi",
-                label: "Logbook",
-                icon: faFileContract,
-                children: [
-                    {
-                        icon: faHome,
-                        to: "/dashboard/logbook",
-                        label: "Dashboard",
-                        permission: "viewLogbook"
-                    },
-                    {
-                        icon: faFeed,
-                        to: "/dashboard/logbook/feed",
-                        label: "Feed",
-                        permission: "viewLogbookFeed",
-                    }
-                ],
-            }
-        ]
-        return data
-    }
+	const elements = (): NavMenuItemProps[] => {
+		const data: NavMenuItemProps[] = [
+			{
+				type: "divider",
+				label: "Info",
+				icon: faHome,
+				requireStaff: true,
+			},
+			{
+				type: "item",
+				label: "Dashboard",
+				to: "/dashboard",
+				icon: faScrewdriverWrench,
+				requireStaff: true,
+			},
+			{
+				type: "item",
+				label: "User Management",
+				to: "/dashboard/user-management",
+				icon: faUser,
+				permission: "viewUser",
+			},
+			{
+				type: "item",
+				label: "Login Whitelist",
+				to: "/dashboard/login-whitelist",
+				icon: faList,
+				permission: "viewWhitelist",
+			},
+			{
+				type: "divider",
+				label: "Logbook",
+				icon: faFileContract,
+			},
+			{
+				type: "multi",
+				label: "Logbook",
+				icon: faFileContract,
+				children: [
+					{
+						icon: faHome,
+						to: "/dashboard/logbook",
+						label: "Monatsübersicht",
+						permission: "viewLogbook",
+					},
+					{
+						icon: faFeed,
+						to: "/dashboard/logbook/feed",
+						label: "Tagesübersicht",
+						permission: "viewLogbookFeed",
+					},
+				],
+			},
+		];
+		return data;
+	};
 
-    return (
-        <NavSidebar elements={elements()} session={session} />
-    )
-}
+	return <NavSidebar elements={elements()} session={session} />;
+};
 
-export default DashNav
+export default DashNav;
