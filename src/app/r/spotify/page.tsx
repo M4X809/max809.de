5c5ce8384865c9e177d824fd1ada453fd5c80524
M4X809 <github@j-pet.de>
2024-11-21 08:46:33 +0100

    Added Open Spotify Redirect

---
+++ src/app/r/spotify/page.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+	metadataBase: new URL("https://max809.com/r/spotify"),
+	title: "Spotify Redirect",
+	description: "Redirect to Spotify",
+	robots: "noindex",
+	icons: {
+		icon:
+			"https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_Green-300x300.png",
+	},
+	openGraph: {
+		title: "Spotify Redirect",
+		description: "Redirect to Spotify",
+		url: "https://max809.com/r/spotify",
+	},
+};
+
+export default async function SpotifyRedirect() {
+	redirect("spotify://");
+}
